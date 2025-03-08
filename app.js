907deb3824d59b3f5500870fcd341e28c6da0dff
Joseph Martinz <carrate.47@gmail.com>
2025-03-07 18:35:38 -0600

    se corriger error al mostrar información de la lista

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function agregarAmigo() {
 
         return;
 }
-
+//función para limpiar la caja de input después de hacer clic en el botón de añadir
 function limpiarCaja(){
     let valorCaja = document.querySelector('#amigo');
     valorCaja.value = '';
@@ -33,19 +33,17 @@ function enlistarAmigos(){
         listaAmigos.appendChild(item);
     }
 }
+/*Se crea función para sortear amigos y se especifica también que si no se ingresa ningún dato en el input
+no aparecerá ninguna expresión o dato en la lista*/
 
 function sortearAmigos(){
-    if(amigos.length === 0){
-        alert('No existen datos de amigos para sortear');
-    }
-
     let amigoElegido = amigos[Math.floor(Math.random()*amigos.length)];
     let resultado = document.getElementById('resultado');
-
-    if (resultado = ''){
+    if(amigos.length === 0){
+        alert('No existen datos de amigos para sortear');
         resultado.innerHTML = ''
     } else{
-    resultado.innerHTML = `Tu amigo secreto es: ${amigoElegido}`;
+        resultado.innerHTML = `Tu amigo secreto es: ${amigoElegido}`;
     }
 
 }
